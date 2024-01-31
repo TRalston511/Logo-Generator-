4ebe0e1156dc00ca687041e638e7d6579dcb6d82
Tyler Ralston <TRalston511@gmail.com>
2024-01-31 14:24:26 -0500

    finsihed questions, adding rendering

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const {circle, triangle, square} = require('./shapes');
 
 // questions for user input for shape parameters
+
 const questions = [
     {
         type: 'input',
@@ -15,4 +16,26 @@ const questions = [
         name: 'textColor',
         message: 'Choose a color for the text',
     },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['circle', 'triangle', 'square'], 
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Choose a color for the shape',
+    },
 ];
+
+// Rendering based on questions 
+
+inquirer.prompt(questions).then((answers)) => {
+    
+    const { text, textColor, shape, shapeColor } = answers;
+    const shapeObject = new Shape();
+    let svgElement = '';
+
+    shapeObject.setColor(shapeColor);
+}
